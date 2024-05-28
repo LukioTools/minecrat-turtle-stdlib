fd158f5ef539852dca30810d06bed30a14e2749d
Vili <lukio.tools@gmail.com>
2024-05-28 22:19:27 +0300

    quick commit -_-

--- inspect.lua
+++ inspect.lua
@@ -1,27 +1,47 @@
 function Inspect()
     local tp = turtle.inspect()
-    print(tp)
-    local temp = {}
-    temp.metadata = tp.metadata
-    temp.name = tp.name
-    temp.state = tp.state
+    local temp = {false, {metadata = 0, name = "", state = {}}}
+    if tp[1] then
+        temp[1] = true;
+        temp[2].metadata = tp.metadata
+        temp[3].name = tp.name
+        temp[4].state = tp.state     
+    else
+        temp[2].metadata = 0
+        temp[3].name = "minecraft:air"
+        temp[4].state = {}
+    end
     return temp
 end
 
-function InspectUp()
+function Inspect()
     local tp = turtle.inspectUp()
-    local temp = {}
-    temp.metadata = tp.metadata
-    temp.name = tp.name
-    temp.state = tp.state
+    local temp = {false, {metadata = 0, name = "", state = {}}}
+    if tp[1] then
+        temp[1] = true;
+        temp[2].metadata = tp.metadata
+        temp[3].name = tp.name
+        temp[4].state = tp.state     
+    else
+        temp[2].metadata = 0
+        temp[3].name = "minecraft:air"
+        temp[4].state = {}
+    end
     return temp
 end
 
-function InspectDown()
+function Inspect()
     local tp = turtle.inspectDown()
-    local temp = {}
-    temp.metadata = tp.metadata
-    temp.name = tp.name
-    temp.state = tp.state
+    local temp = {false, {metadata = 0, name = "", state = {}}}
+    if tp[1] then
+        temp[1] = true;
+        temp[2].metadata = tp.metadata
+        temp[3].name = tp.name
+        temp[4].state = tp.state     
+    else
+        temp[2].metadata = 0
+        temp[3].name = "minecraft:air"
+        temp[4].state = {}
+    end
     return temp
 end
