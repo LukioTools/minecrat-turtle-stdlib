573dc75d4363f10111a703fee14958da6bbda17b
Vili <lukio.tools@gmail.com>
2024-05-28 22:25:44 +0300

    quick commit -_-

--- inspect.lua
+++ inspect.lua
@@ -2,7 +2,7 @@ function Inspect()
     local tp = turtle.inspect()
     local temp = {false, {metadata = 0, name = "", state = {}}}
     print(temp[1])
-    if tp[1] then
+    if tp[1] == "true" then
         temp[1] = true;
         temp[2].metadata = tp.metadata
         temp[3].name = tp.name
@@ -18,7 +18,7 @@ end
 function InspectUp()
     local tp = turtle.inspectUp()
     local temp = {false, {metadata = 0, name = "", state = {}}}
-    if tp[1] then
+    if tp[1] == "true" then
         temp[1] = true;
         temp[2].metadata = tp.metadata
         temp[3].name = tp.name
@@ -34,7 +34,7 @@ end
 function InspectDown()
     local tp = turtle.inspectDown()
     local temp = {false, {metadata = 0, name = "", state = {}}}
-    if tp[1] then
+    if tp[1] == "true" then
         temp[1] = true;
         temp[2].metadata = tp.metadata
         temp[3].name = tp.name
