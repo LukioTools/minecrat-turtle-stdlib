6e368557dc11c14080303e2813150733f774af5f
Vili <lukio.tools@gmail.com>
2024-05-28 17:10:36 +0300

    a

--- moveTo.lua
+++ moveTo.lua
@@ -1,6 +1,78 @@
+local function Forward(l)
+end
 
---current pos (x,y,z), current rotation (x,y), future position (x,y,z), future rotation (x,y)
-function moveTo(c_px,c_py,c_pz, c_rx, c_ry,f_x,f_y,f_z, f_rx, f_ry)
+local function Backward(l)
+end
+
+local function Upward(l)
+end
+
+local function Downward(l)
+end
+
+local function Rotate(l)
+end
+
+local function SetRotation(l)
+    
+end
+
+local function rotateToDirection(cr,fr)
+    local rotationAmount =  f_ry - c_ry
+    if rotationAmount > 0 then
+        for i = 1, rotationAmount, 1 do
+            turtle.turnRight(i) 
+        end
+    elseif rotationAmount <0 then 
+        for i = 1, rotationAmount, 1 do
+            turtle.turnLeft(i) 
+        end
     end
+end
+
+-- y = korkeus, z = eteenpäin, x = sivulle
+
+-- 1 forward
+-- 2 right
+-- 3 backward
+-- 4 left
+
+-- global positions
+--current rotation, current pos (x,y,z), future position (x,y,z)
+function moveTo(d, c_x,c_y,c_z, f_x, f_y, f_z)
+    local rel_x = f_x-c_x
+    local rel_z = f_z-c_z
+    local rel_y = f_y-c_y
+    
+    -- forward
+    if d == 1 then
+        Forward(rel_z)
+        Upward(rel_y)
+        SetRotation(2)
+        Forward(rel_x)        
+
+    -- right
+    elseif d == 2 then
+        Forward(rel_x)
+        Upward(rel_y)
+        SetRotation(1)
+        Forward(rel_z)
+
+    -- backward
+    elseif d == 3 then
+        Forward(-rel_z)
+        Upward(rel_y)
+        SetRotation(2)
+        Forward(rel_x)
+
+    --left
+    elseif d == 4 then
+        Forward(-rel_x)
+        Upward(rel_y)
+        SetRotation(1)
+        Forward(rel_z)
+    end
+
+end
 
 
