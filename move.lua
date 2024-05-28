47405baf9591ae47dfead4c21912a107375c74cd
PikkuHurja <pikku.hurja@gmail.com>
2024-05-28 16:33:15 +0300

    quick commit -_-

---
+++ move.lua
@@ -0,0 +1,20 @@
+require "origin"
+
+function forward()
+    return turtle.forward();
+end
+
+function backward()
+    return turtle.back();
+end
+
+function upward()
+    return turtle.back();
+end
+
+function downward()
+    return turtle.back();
+end
+
+
+
