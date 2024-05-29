4575c7d50460f2a0653c836be1a847d33829d2e3
Vili <lukio.tools@gmail.com>
2024-05-29 18:41:32 +0300

    quick commit -_-

--- clear_area.lua
+++ clear_area.lua
@@ -12,14 +12,14 @@ print(sizeX, sizeY, sizeZ)
 
 for i = 0, sizeY, 1 do     
     for j = 0, sizeX, 1 do
-        moveTo({i,j,0})
+        moveTo({j,i,0})
         SetRotation(1)
         for k = 0, sizeZ, 1 do
             Dig("left")
             Forward(1)
             AutoRefuel(16, 1)
         end
-        moveTo({i,j,0})
+        moveTo({j,i,0})
         
     end
 end
