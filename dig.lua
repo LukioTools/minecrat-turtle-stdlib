5ca8a67fd1e665b2e3311174312464a6f9d780f2
Vili <lukio.tools@gmail.com>
2024-05-29 17:14:53 +0300

    quick commit -_-

---
+++ dig.lua
@@ -0,0 +1,15 @@
+-- Breaks the block in front. With hoe: tills the dirt in front of it. 
+function Dig(side)
+    local success = false
+    success = turtle.Dig()
+end
+
+function DigUp(side)
+    local success = false
+    success = turtle.DigUp()
+end
+
+function DigDown(side)
+    local success = false
+    success = turtle.DigDown()
+end
