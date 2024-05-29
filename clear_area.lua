da308b29aaa253c5659114fa416030f7ab5a94d7
Vili <lukio.tools@gmail.com>
2024-05-29 18:33:01 +0300

    quick commit -_-

--- clear_area.lua
+++ clear_area.lua
@@ -11,12 +11,14 @@ print(Targ[1], Targ[2], Targ[3])
 print(sizeX, sizeY, sizeZ)
 
 for i = 1, sizeY, 1 do     
-    for i = 1, sizeX, 1 do
-        for i = 1, sizeZ, 1 do
+    for j = 1, sizeX, 1 do
+        moveTo(i,j,0)
+        for k = 1, sizeZ, 1 do
             Forward(1)
             Dig("left")
             AutoRefuel(16, 1)
         end
+        moveTo(i,j,0)
     end
 end
 
