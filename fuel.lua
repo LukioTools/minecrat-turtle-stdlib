38e80589d55b8c1049caf13820b572d527971684
Vili <lukio.tools@gmail.com>
2024-05-29 16:51:21 +0300

    quick commit -_-

---
+++ fuel.lua
@@ -0,0 +1,41 @@
+function EndGetRefuelLevel()
+    local result = 0
+    result = turtle.getFuelLevel()
+    return result
+end
+
+function GetFuelLimit()
+    local result = 0
+    result = turtle.getFuelLimit()
+    return result
+end
+
+function Refuel()
+    local result = false
+    result = turtle.refuel()
+    return result
+end
+
+-- How many items it consumes,amount of slots
+function AutoRefuel(slots, fuelAmount)
+    local success = false
+    if fuelAmount then
+        for i = 1, slots, 1 do
+            local wasAble = turtle.refuel(fuelAmount)
+            if wasAble then
+                success = true
+                break
+            end
+        end
+    else
+        for i = 1, slots, 1 do
+            local wasAble = turtle.refuel()
+            if wasAble then
+                success = true
+                break
+            end
+        end
+    end
+    return success
+
+end
