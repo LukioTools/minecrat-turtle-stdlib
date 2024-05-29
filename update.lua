6d7b7e8226b5fdd58140905873bc27a41300c8c9
Vili <lukio.tools@gmail.com>
2024-05-29 17:04:35 +0300

    quick commit -_-

--- update.lua
+++ update.lua
@@ -2,48 +2,37 @@ require "https"
 
 local urls = {
     {
-        url="http://0.0.0.0/move.lua",
         filename="move.lua"
     },
     {
-        url="http://0.0.0.0/moveTo.lua",
         filename="moveTo.lua"
     },
     {
-        url="http://0.0.0.0/stdlib.lua",
         filename="stdlib.lua"
     },
     {
-        url="http://0.0.0.0/update.lua",
         filename="update.lua"
     },
     {
-        url="http://0.0.0.0/inspect.lua",
         filename="inspect.lua"
     },
     {
-        url="http://0.0.0.0/inventory.lua",
         filename="inventory.lua"
     },
     {
-        url="http://0.0.0.0/comparation.lua",
         filename="comparation.lua"
     },
     {
-        url="http://0.0.0.0/fuel.lua",
         filename="fuel.lua"
     },
     {
-        url="http://0.0.0.0/suck.lua",
         filename="suck.lua"
     },
     {
-        url="http://0.0.0.0/drop.lua",
         filename="drop.lua"
     },
 }
 
 for index, value in ipairs(urls) do
-    print("filename: " .. value.filename .. ", url: ".. value.url);
-    WriteTo(value.filename, GetHTTP(value.url));
+    WriteTo(value.filename, GetHTTP("http://0.0.0.0/"+value.filename));
 end
