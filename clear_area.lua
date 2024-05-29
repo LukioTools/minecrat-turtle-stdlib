64726766dac8399a73faf902b31c176bbaed8f33
Vili <lukio.tools@gmail.com>
2024-05-29 18:38:16 +0300

    quick commit -_-

--- clear_area.lua
+++ clear_area.lua
@@ -10,15 +10,17 @@ local sizeZ = tonumber(Targ[3])
 print(Targ[1], Targ[2], Targ[3])
 print(sizeX, sizeY, sizeZ)
 
-for i = 1, sizeY, 1 do     
-    for j = 1, sizeX, 1 do
+for i = 0, sizeY, 1 do     
+    for j = 0, sizeX, 1 do
         moveTo({i,j,0})
-        for k = 1, sizeZ, 1 do
+        SetRotation(1)
+        for k = 0, sizeZ, 1 do
             Dig("left")
             Forward(1)
             AutoRefuel(16, 1)
         end
         moveTo({i,j,0})
+        
     end
 end
 
