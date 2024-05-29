2d90f2ca48bc235d460390f55bfdaef4f2b8c4d0
Vili <lukio.tools@gmail.com>
2024-05-29 17:48:22 +0300

    quick commit -_-

--- dig.lua
+++ dig.lua
@@ -1,18 +1,18 @@
 -- Breaks the block in front. With hoe: tills the dirt in front of it. 
 function Dig(hand)
     local success = false
-    success = turtle.Dig(hand)
+    success = turtle.dig(hand)
     return success
 end
 
 function DigUp(hand)
     local success = false
-    success = turtle.DigUp(hand)
+    success = turtle.digUp(hand)
     return success
 end
 
 function DigDown(hand)
     local success = false
-    success = turtle.DigDown(hand)
+    success = turtle.digDown(hand)
     return success
 end
